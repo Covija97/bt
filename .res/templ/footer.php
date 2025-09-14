d880d648d2eec85a308ed04639dd42896eba491e
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-09-14 19:45:12 +0200

    upd

---
+++ .res/templ/footer.php
@@ -0,0 +1,6 @@
+    <script src="/badentracker/.res/js/scripts.js?v=2" defer></script>
+    <footer>
+        <p>BadenTracker 2025 by <b>Javier Coronel Vides</b></p>
+    </footer>
+</body>
+</html>
