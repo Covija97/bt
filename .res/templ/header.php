6e10959df96d79b5abef3aa48ab91391e163ce4e
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-09-14 19:39:32 +0200

    Fix paths in header.php for CSS and icon resources

--- .res/templ/header.php
+++ .res/templ/header.php
@@ -8,13 +8,13 @@
             - <?php echo $title ?>
         <?php endif ?>
     </title>
-    <link rel="stylesheet" href="/bt_local/.res/css/header.css">
+    <link rel="stylesheet" href="/bt/.res/css/header.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="icon" href="/bt_local/.res/icon/bt_icon.svg">
+    <link rel="icon" href="/bt/.res/icon/bt-icon.svg">
 </head>
 <body>
     <header>
-        <a href="/bt_local/.res/icon/" class="logo"></a>
+        <a href="/bt/.res/icon/" class="logo"></a>
     </header>
 </body>
 </html>
