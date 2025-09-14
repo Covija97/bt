0100d112fcc896bb6808b369402f15d57db612b8
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-09-14 19:49:35 +0200

    Fix resource paths in header.php for consistency

--- .res/templ/header.php
+++ .res/templ/header.php
@@ -8,11 +8,11 @@
             - <?php echo $title ?>
         <?php endif ?>
     </title>
-    <link rel="stylesheet" href="/bt_local/.res/css/header.css">
+    <link rel="stylesheet" href="/bt/.res/css/header.css">
+    <link rel="icon" href="/bt/.res/icon/bt_icon.svg">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="icon" href="/bt_local/.res/icon/bt_icon.svg">
 </head>
 <body>
     <header>
-        <a href="/bt_local/.res/icon/" class="logo"></a>
+        <a href="/bt/.res/icon/" class="logo"></a>
     </header>
